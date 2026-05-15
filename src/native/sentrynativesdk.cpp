ca967fa5a220b0dff26e6d46d39c7d7f599cf665
J-P Nurmi <jpnurmi@gmail.com>
2026-05-15 22:38:17 +0200

    SENTRY_ATTACHMENT_TYPE_VIEW_HIERARCHY

--- src/native/sentrynativesdk.cpp
+++ src/native/sentrynativesdk.cpp
@@ -12,7 +12,6 @@
 
 extern "C" {
 #include <include/sentry.h>
-#include <src/sentry_attachment.h>
 }
 
 #include <QtCore/qbytearray.h>
@@ -485,8 +484,7 @@ void attachViewHierarchyToScope(sentry_scope_t *scope, const QByteArray &json)
         return;
     }
 
-    // ### TODO: sentry_attachment_set_type(SENTRY_ATTACHMENT_TYPE_VIEW_HIERARCHY)
-    attachment->type = VIEW_HIERARCHY;
+    sentry_attachment_set_type(attachment, SENTRY_ATTACHMENT_TYPE_VIEW_HIERARCHY);
     setNativeAttachmentContentType(attachment, QStringLiteral("application/json"));
 }
 
